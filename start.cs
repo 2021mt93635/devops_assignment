9f23ed5bdef2042de616afc12248724be73f3cd3
devopsUser93 <devopsuser93@gmail.com>
2022-11-12 23:19:13 +0530

    commit to see user change to devops user

--- start.cs
+++ start.cs
@@ -4,7 +4,7 @@ void main(char[] args)
     int b = 20;
     int c = a+b;
     // added print statement
-    // comment added by devops user
+    // comment added by devops user - test
     Console.WriteLine(c);
     Console.ReadLine();
 }
